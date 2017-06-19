57ff24c36e0be32b84950c23cae28708b8828456
Dr. Jonathan Bradley <bradley1@vt.edu>
2017-06-19 10:51:15 -0400

    Update url to library feed

--- controllers/instagram.js
+++ controllers/instagram.js
@@ -2,7 +2,7 @@ exports.getResults = function (callback) {
   var request = require('request')
   var aparse = require('../parsers/instagram-parser.js').parse
 
-  var submitUrl = 'https://www.instagram.com/virginia.tech/media/'
+  var submitUrl = 'https://www.instagram.com/vtlibraries/media/'
 
   request(submitUrl, function (error, response, body) {
     if (!error && response.statusCode === 200) {
